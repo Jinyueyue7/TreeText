b394e381ff228ce9708834f53aad56286e8e0183
Jinyueyue7 <921386013@qq.com>
2016-04-12 16:39:27 +0800

    Revert "Revert "Initial Commit""
    This reverts commit 1a8342608cf633d3a6286324b06240b0db9b92a4.

---
+++ TreeText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TreeText
+//
+//  Created by 金悦悦 on 16/4/8.
+//  Copyright © 2016年 金悦悦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TreeTextUITests/TreeTextUITests.m
@@ -0,0 +1,40 @@
+//
+//  TreeTextUITests.m
+//  TreeTextUITests
+//
+//  Created by 金悦悦 on 16/4/8.
+//  Copyright © 2016年 金悦悦. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TreeTextUITests : XCTestCase
+
+@end
+
+@implementation TreeTextUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
